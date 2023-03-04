2104b5cca188a96b5be7de060523e3828784b286
VaiTon <eyadlorenzo@gmail.com>
2023-03-04 14:38:10 +0100

    Display burger menu on mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import '../app.scss';
+	let menuOpen = false;
+	function toggleMenu() {
+		menuOpen = !menuOpen;
+	}
 </script>
 
 <nav class="container navbar" aria-label="main navigation">
@@ -7,8 +11,21 @@
 		<a class="navbar-item" href="/">
 			<h2>Aule@Unibo</h2>
 		</a>
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<!-- svelte-ignore a11y-missing-attribute -->
+		<a
+			role="button"
+			class="navbar-burger"
+			aria-label="menu"
+			aria-expanded="false"
+			on:click={toggleMenu}
+		>
+			<span aria-hidden="true" />
+			<span aria-hidden="true" />
+			<span aria-hidden="true" />
+		</a>
 	</div>
-	<div class="navbar-menu">
+	<div class="navbar-menu" class:is-active={menuOpen}>
 		<div class="navbar-start">
 			<div class="navbar-item">
 				<a href="/matematica">Matematica</a>
