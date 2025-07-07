6e1ce22b2dd8f17ffc956007aaed22e42d5b2b71
VaiTon <eyadlorenzo@gmail.com>
2025-07-07 22:36:48 +0200

    Remove unused imports and SSR export from page module

--- src/routes/cal/[calId]/[aulaId]/+page.ts
+++ src/routes/cal/[calId]/[aulaId]/+page.ts
@@ -1,8 +1,6 @@
-import { getAule, getImpegni } from '$lib/api';
+import { getAule } from '$lib/api';
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
-
-export const ssr = false;
 
 export const load: PageLoad = async ({ fetch, params }) => {
 	const aule = await getAule(fetch, params.calId, {
