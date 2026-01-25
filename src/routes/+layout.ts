bbee7576f65579300bb382a3bf2abb6a91dadd02
VaiTon <eyadlorenzo@gmail.com>
2026-01-25 23:48:53 +0100

    chore: Add comments to clarify why SSR is disabled

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,4 @@
+// Disable server-side rendering for the entire application.
+// This way all requests are handled on the client side, and
+// we don't risk upsetting any API with rate limiting issues.
 export const ssr = false;
